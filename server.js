41f24155e8074cc98952de2d527d780aab8c7c0f
thejasri1 <tejasmadi1@example.com>
2023-04-01 21:46:45 +0530

    socket.io added

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const AuthenticationPage = require('./Routes/Authentication')
 const MessagesPage = require('./Routes/Messages')
 const bodyParser = require("body-parser")
 const cors = require('cors')
+const http = require("http")
 const dotenv = require('dotenv');
 dotenv.config();
+const { Server } = require("socket.io")
 
 const express = require('express')
 const router = express()
@@ -18,22 +20,35 @@ router.use(express.json())
 router.use(cors())
 
 //Conneting with external files
-router.use(bodyParser.json({limit:'10mb'}))
-router.use(bodyParser.urlencoded({limit:'10mb',extended:true}))
-router.use('/',AccountsPage)
-router.use('/auth',AuthenticationPage)
-router.use('/messages',MessagesPage)
+router.use(bodyParser.json({ limit: '10mb' }))
+router.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
+router.use('/', AccountsPage)
+router.use('/auth', AuthenticationPage)
+router.use('/messages', MessagesPage)
 
+const server = http.createServer(router)
 
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
+})
+io.on("connection", (socket) => {
+    console.log(` User Connected : ${socket.id}`)
+    socket.on("send_message", (data) => {
+        socket.broadcast.emit("recieve_message", data)
+    })
+})
 //Mongo DB connection function
-mongoose.connect(DB_URL, {useNewUrlParser: true,useUnifiedTopology: true}).then(async (db, err) => {
-    if(err) throw err
+mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(async (db, err) => {
+    if (err) throw err
     else console.log("connected")
 })
 
-const PORT = process.env.NODE_ENV === "production" ? process.env.PRODUCTION_URL:process.env.PORT
+const PORT = process.env.NODE_ENV === "production" ? process.env.PRODUCTION_URL : process.env.PORT
 
 //server connectionprocess.env.NODE_ENV 
-router.listen(PORT,(req,res)=>{
+server.listen(PORT, (req, res) => {
     console.log(`http://localhost:${PORT}`)
 })
